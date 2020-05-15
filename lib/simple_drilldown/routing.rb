0dd3fb18d37ea95286d5c98a61159a31edfd9764
Uwe Kubosch <uwe@datek.no>
2020-05-15 12:14:59 +0200

    Add SimpleDrilldown::Routing#draw_drilldown method for easy routing to a drilldown controller

---
+++ lib/simple_drilldown/routing.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module SimpleDrilldown
+  module Routing
+    def draw_drilldown(path, controller = path)
+      get "#{path}(.:format)" => "#{controller}#index", as: path
+      scope path do
+        %i[excel_export html_export index].each do |action|
+          get "#{action}(/:id)(.:format)", controller: controller, action: action
+        end
+      end
+    end
+  end
+end
