349ca91a857a3ce52abf1ca93548dce9ba5589cb
Uwe Kubosch <uwe@datek.no>
2020-06-20 08:33:37 +0200

    Add Post model to the dummy test app

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -12,10 +12,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_618_145_725) do
+ActiveRecord::Schema.define(version: 20_200_620_062_540) do
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title', null: false
+    t.text 'body', null: false
+    t.integer 'user_id', null: false
+    t.datetime 'published_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_posts_on_user_id'
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
+
+  add_foreign_key 'posts', 'users'
 end
