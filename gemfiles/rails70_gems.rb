3828d9d29522df24bd15cb2cef701b5a1673a897
Uwe Kubosch <uwe@datek.no>
2022-06-29 09:36:00 +0200

    Correct path to Gemfile

--- gemfiles/rails70_gems.rb
+++ gemfiles/rails70_gems.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-gems = "#{File.dirname __dir__}/Gemfile"
+gems = File.expand_path('../Gemfile', __dir__)
 eval File.read(gems), binding, gems # rubocop: disable Security/Eval
 
 gem 'rails', '~> 7.0.0'
