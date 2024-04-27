d61bc072cb68e16dbfab09492220317239e2baa6
Uwe Kubosch <uwe@datek.no>
2024-04-27 11:24:58 +0200

    Require Rails 7.0+

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -10,7 +10,7 @@ require 'simple_drilldown'
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
